4dd1f6c51d1e88194cde473affa3f4124ba48f6a
范清源 <maynear_ppp@fanqingyuandeMacBook-Pro.local>
2020-07-17 19:03:43 +0800

    splittext start

--- aeSubtitleScripts.jsx
+++ aeSubtitleScripts.jsx
@@ -1,125 +1,58 @@
-(function importText(thisObj) {
+(function splitText(thisObj) {
   //////////info////////////
-  var scriptName = "importText ver1.0";
+  var scriptName = "splitText";
   var alertTitle = "Warning:";
 
-  var file = new File();
-  var _selected = false;
-  var _option = 0;
+  var curComp = app.project.activeItem;
 
   function throwError(msg) {
     alert(msg, alertTitle);
   }
 
-  function buildUI(thisObj) {
-    var palette;
+  // function buildUI(thisObj) {
+  //   var palette;
 
-    if (thisObj instanceof Panel) {
-      palette = thisObj;
-    } else {
-      palette = new Window("palette", "ImportText", undefined, {
-        resizeable: true,
-      });
-    }
+  //   if (thisObj instanceof Panel) {
+  //     palette = thisObj;
+  //   } else {
+  //     palette = new Window("palette", "", undefined, { resizeable: true });
+  //   }
 
-    palette.orientation = "column";
-    palette.alignChildren = "left";
+  //   palette.orientation = "";
+  //   palette.alignChildren = "";
 
-    // File Select Module
-    var textImportBox = palette.add("group", undefined, "textImportBox");
-    textImportBox.orientation = "row";
-    var fileSelectBox = textImportBox.add(
-      "edittext",
-      undefined,
-      "Select your file"
-    );
-    fileSelectBox.size = [160, 20];
-    var fileSelectButton = textImportBox.add("button", undefined, "Select");
-    fileSelectButton.helpTip = "Select a .txt file.";
-    fileSelectButton.onClick = selectFile;
-    // Option Module
-    var optionGroup = palette.add("group", undefined, "optionGroup");
-    optionGroup.orientation = "row";
-    var selectGroup = optionGroup.add("group", undefined, "selectGroup");
-    selectGroup.orientation = "column";
-    selectGroup.alignChildren = "left";
-    var checkBox01 = selectGroup.add("checkbox", undefined, "line by line");
-    checkBox01.value = false;
-    var checkBox02 = selectGroup.add("checkbox", undefined, "interlace");
-    checkBox02.value = false;
-    var buttonGroup = optionGroup.add("group", undefined, "buttonGroup");
-    buttonGroup.orientation = "column";
-    var createTextButton = buttonGroup.add("button", undefined, "Create Text!");
-    createTextButton.onClick = function create() {
-      if (checkBox01.value == true && checkBox02.value != true) {
-        _option = 1;
-      } else if (checkBox01.value != true && checkBox02.value == true) {
-        _option = 2;
-      } else if (checkBox01.value == true && checkBox02.value == true) {
-        _option = -1;
-      }
-      main();
-    };
-
-    if (palette instanceof Window) {
-      palette.layout.layout(true);
-      return palette;
-    } else {
-      throwError("error");
-    }
-
-    function selectFile() {
-      file = file.openDlg("Open your file", "Acceptable Files:*.txt");
-      fileSelectBox.text = file.fsName;
-      _selected = true;
-    }
-  }
+  //   if (palette instanceof Window) {
+  //     palette.layout.layout(true);
+  //     return palette;
+  //   } else {
+  //     throwError("error");
+  //   }
+  // }
 
   function main() {
-    var curComp = app.project.activeItem;
     if (!(curComp instanceof CompItem)) {
       throwError("Please open a comp!");
       return;
     }
+    var textLayer = curComp.selectedLayers[0];
 
     app.beginUndoGroup(scriptName);
-    createTextLayers(curComp);
+    split(textLayer);
     app.endUndoGroup;
   }
 
-  function createTextLayers(curComp) {
-    if (!_selected) {
-      throwError("Please select a file!");
-      return;
-    }
-    if (_option == 0) {
-      throwError("Please select an option!");
-      return;
-    } else if (_option == -1) {
-      throwError("Please just select one option!");
-      return;
-    }
-    var fileData = readTXT();
-    for (var i = Math.floor(_option / 2); i < fileData.length; i += _option) {
-      curComp.layers.addText(fileData[i]);
+  function split(textLayer) {
+    var sourceText = textLayer.property("Source Text").value.text;
+    var sourceTextArray = sourceText.split("");
+    for (var i = 0; i < sourceTextArray.length; i++) {
+      curComp.layers.addText(sourceTextArray[i]);
     }
   }
 
-  function readTXT() {
-    var currentLine;
-    var textArray = [];
-    file.open("r");
-    while (!file.eof) {
-      currentLine = file.readln();
-      textArray.push(currentLine);
-    }
-    file.close();
-    return textArray;
-  }
-
-  var UI = buildUI(thisObj);
-  if (UI != null && UI instanceof Window) {
-    UI.center();
-    UI.show();
-  }
+  // var UI = buildUI(thisObj);
+  // if (UI != null && UI instanceof Window) {
+  //   UI.center();
+  //   UI.show();
+  // }
+  main();
 })(this);
