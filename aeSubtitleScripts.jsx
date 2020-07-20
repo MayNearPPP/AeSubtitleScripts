a3878e552f4fe7595bec70bea6f36547fd20a6e4
MayNearPPPonMac <maynearppp@gmail.com>
2020-07-20 11:37:51 +0800

    splitText finished

--- aeSubtitleScripts.jsx
+++ aeSubtitleScripts.jsx
@@ -9,25 +9,32 @@
     alert(msg, alertTitle);
   }
 
-  // function buildUI(thisObj) {
-  //   var palette;
+  function buildUI(thisObj) {
+    var palette;
 
-  //   if (thisObj instanceof Panel) {
-  //     palette = thisObj;
-  //   } else {
-  //     palette = new Window("palette", "", undefined, { resizeable: true });
-  //   }
+    if (thisObj instanceof Panel) {
+      palette = thisObj;
+    } else {
+      palette = new Window("palette", scriptName, undefined, {
+        resizeable: true,
+      });
+    }
 
-  //   palette.orientation = "";
-  //   palette.alignChildren = "";
+    palette.orientation = "column";
+    palette.alignChildren = "center";
 
-  //   if (palette instanceof Window) {
-  //     palette.layout.layout(true);
-  //     return palette;
-  //   } else {
-  //     throwError("error");
-  //   }
-  // }
+    var split = palette.add("button", undefined, "Split Text!");
+    split.onClick = function create() {
+      main();
+    };
+
+    if (palette instanceof Window) {
+      palette.layout.layout(true);
+      return palette;
+    } else {
+      throwError("error");
+    }
+  }
 
   function main() {
     if (!(curComp instanceof CompItem)) {
@@ -44,17 +51,21 @@
   function split(textLayer) {
     var sourceText = textLayer.property("Source Text").value.text;
     var sourceTextArray = sourceText.split("");
-    // for (var i = 0; i < sourceTextArray.length; i++) {
-    //   curComp.layers.addText(sourceTextArray[i]);
-    // }
+    var nullLayer = curComp.layers.addNull();
     for (var i = 0; i < sourceTextArray.length; i++) {
       var textSplitLayer = curComp.layers.addText(sourceTextArray[i]);
+      textSplitLayer.setParentWithJump(nullLayer);
       var position = textSplitLayer.property("Position");
       var positionValue = position.value;
       var distance = calculate(textLayer, i);
-      positionValue[0] = positionValue[0] + distance;
+      positionValue = [distance, 0];
       position.setValue(positionValue);
+      textSplitLayer.selected = false;
     }
+    nullLayer
+      .property("Position")
+      .setValue(textLayer.property("Position").value);
+    nullLayer.selected = true;
   }
 
   function changeParagraph(textLayer) {
@@ -79,13 +90,13 @@
     newTextLayer.property("Source Text").setValue(sourceText);
     data.push(newTextLayer.sourceRectAtTime(time, true).left);
     var distance = data[2] - data[1];
+    newTextLayer.remove();
     return distance;
   }
 
-  // var UI = buildUI(thisObj);
-  // if (UI != null && UI instanceof Window) {
-  //   UI.center();
-  //   UI.show();
-  // }
-  main();
+  var UI = buildUI(thisObj);
+  if (UI != null && UI instanceof Window) {
+    UI.center();
+    UI.show();
+  }
 })(this);
