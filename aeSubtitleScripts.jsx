88a12a0490b2909802df828eb536d648fe7619a5
MayNearPPPonMac <maynearppp@gmail.com>
2020-07-22 16:28:57 +0800

    test

--- aeSubtitleScripts.jsx
+++ aeSubtitleScripts.jsx
@@ -1,11 +1,3 @@
-(function split() {
-  function createWindow() {
-    var w = new Window("palette");
-    var m = w.add("statictext");
-    m.text = "hello";
-    return w;
-  }
-
-  var win = createWindow();
-  win.show();
+(function name(params) {
+  alert("hello");
 })();
